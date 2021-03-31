cdaffd25e63825f9ae7dbc0d8e2ca1b6d71e1c8a
Michal Kurleto <michal.kurleto@gmail.com>
2021-03-31 21:45:45 +0200

    FOODSHARIN-8 change config loading

--- FoodsharingBackend/foodsharing/__init__.py
+++ FoodsharingBackend/foodsharing/__init__.py
@@ -1,36 +1,23 @@
 from flask import Flask
 import os
 from flask_sqlalchemy import SQLAlchemy
-
+from foodsharing.config import Config
 
 db = SQLAlchemy()
 
 
-def create_app(test_config=None):
+def create_app(config_class=Config):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    app = Flask(__name__)
+    app.config.from_object(config_class)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    
-    # init 
+    # init database
     db.init_app(app)
     
-    # importing blueprints
+    # import blueprints
     from .views.home import home
     
-    # registering blueprints
+    # register blueprints
     app.register_blueprint(home, url_prefix="/")
 
     return app
--- FoodsharingBackend/foodsharing/config.py
+++ FoodsharingBackend/foodsharing/config.py
@@ -1,7 +1,8 @@
 class Config:
+    DEBUG=True
     SECRET_KEY = 'dev'
     SQLALCHEMY_DATABASE_URI = "postgresql://user:password@localhost/spaceshipDB" # TODO
-    
+
     # Real config example:
     # SECRET_KEY = os.environ.get('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
